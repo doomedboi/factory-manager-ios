2abf2c95dc1106c7d8c66956665f65a7d81f46d0
def <def@mail.ru>
2022-01-05 13:45:44 +1000

    Initial Commit

---
+++ factory-manager-iosUITests/factory_manager_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  factory_manager_iosUITestsLaunchTests.swift
+//  factory-manager-iosUITests
+//
+//  Created by soFuckingHot on 05.01.2022.
+//
+
+import XCTest
+
+class factory_manager_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
